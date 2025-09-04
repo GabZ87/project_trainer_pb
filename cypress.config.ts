449a224fe7872d8035919950ba6dfa787a318b33
Gab Zsapka <62764670+GabZ87@users.noreply.github.com>
2025-09-04 11:25:34 +0100

    basic setup

---
+++ cypress.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "cypress";
+
+export default defineConfig({
+  e2e: {
+    setupNodeEvents(on: any, config: any) {
+      // implement node event listeners here
+    },
+  },
+});
