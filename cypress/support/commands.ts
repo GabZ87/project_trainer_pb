3dc7f54e517ba5880ea744e73e4e90934cc04910
Vote-pBlue <iratellama@gmail.com>
2025-09-16 15:15:49 +0100

    day 2 in the big brother house - hard to describe

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -24,10 +24,9 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
-import { faker } from '@faker-js/faker';
-import { userData } from '../fixtures/testdata';
-import { User } from './types/user';
 
+import { userData } from '../fixtures/testdata';
+import { User } from '../support/types/user';
 // below is registerUser command
 
 import { userFactory } from '../support/utils/user-factory';
@@ -46,6 +45,7 @@ function registerUserHelper() {
   cy.get('body').then($body => {
     if ($body.text().includes('This username already exists.')) {
       cy.log('Username exists, retrying registration...');
+      // Recursively try again
       registerUserHelper();
     } else {
       cy.contains('Your account was created successfully. You are now logged in.').should('be.visible');
@@ -54,27 +54,5 @@ function registerUserHelper() {
 }
 
 Cypress.Commands.add('registerUser', () => {
-  registerUserHelper();
+  return registerUserHelper();
 });
-//   cy.get('input.button').contains('Log In').should('exist');
-//   cy.get('a').contains('Register').should('be.visible').click();
-
-//     Object.values(userData).forEach((field: { input: string; type: string }) => {
-//     cy.get(`input[id="${field.input}"]`).type(user[field.type]);
-//   });
-
-//   cy.get('input.button').contains('Register').click();
-// });
-
-
-// above is registerUser command
-// below is login command
-
-Cypress.Commands.add('login', (username: string, password: string) => {
-  cy.visit('/login'); // or your login page URL
-  cy.get('input[name="username"]').type(username);
-  cy.get('input[name="password"]').type(password);
-  cy.get('button[type="submit"]').click();
-});
-
-// above is login command
--- cypress/support/global.d.ts
+++ cypress/support/global.d.ts
@@ -1,24 +1,7 @@
-import { User } from "./types/user";
+import { User } from "cypress/support/types/user";
 
 declare namespace Cypress {
-
   interface Chainable {
-    login(username: string, password: string): Chainable<void>;
-  }
-
-  interface Chainable {
-    /**
-     * Custom command to register a user and save details
-     * @example cy.registerUser()
-     */
     registerUser(user: User): Chainable<User>;
-  }
-
-  interface Chainable {
-    /**
-     * Custom task to get user files as a string array
-     * @example cy.task('getUserFiles')
-     */
-    task(event: 'getUserFiles'): Chainable<string[]>;
   }
 }
